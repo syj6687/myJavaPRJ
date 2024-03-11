bcdd5c08fcc34bf0028bd5121cb3cac3e02dae85
syj6687 <syjsyj6687@naver.com>
2024-03-11 15:14:13 +0900

    소스수정

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,10 +3,11 @@ public class Hello {
 
 
 
-    public static void main(String[] args){
+    public static void main(String[] args){//main 함수입니다.
 
         System.out.println("Hello!! My First Java Coding!!");
 
         System.out.println("Edit Java Code!!");
+
     }
 }
