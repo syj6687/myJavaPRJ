696e92a983abc421bea057c8a3f920da8ee431c5
syj6687 <syjsyj6687@naver.com>
2024-03-18 13:32:18 +0900

    2번째 파일

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -5,9 +5,10 @@ public class Hello {
 
     public static void main(String[] args){//main 함수입니다.
 
-        System.out.println("Hello!! My First Java Coding!!");
+        int a = 100;
+        int b = 200;
+        int c = a + b;
 
-        System.out.println("Edit Java Code!!");
-
+        System.out.println(a + " + " + b + " = " +  c);
     }
 }
