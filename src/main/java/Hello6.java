8e90bdd60c7f630287aa4f2c977afed9f0f1e1be
syj6687 <syjsyj6687@naver.com>
2024-03-25 13:25:57 +0900

    2번째 파일

--- src/main/java/Hello6.java
+++ src/main/java/Hello6.java
@@ -1,2 +1,10 @@
 public class Hello6 {
+    public static void main(String[] args) {
+        String var1 = "작은따움표는 \' 모양입니다.";
+        String var2 = "큰따움표는 \" 모양입니다.";
+
+        System.out.println(var1);
+        System.out.println(var2);
+
+    }
 }
