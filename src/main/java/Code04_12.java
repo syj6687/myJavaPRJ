0412707a1113a002259e4515da5e7c51dfc1ae67
syj6687 <syjsyj6687@naver.com>
2024-03-26 10:54:25 +0900

    2번째 파일

--- src/main/java/Code04_12.java
+++ src/main/java/Code04_12.java
@@ -1,11 +1,13 @@
 public class Code04_12 {
     public static void main(String[] args) {
         String ss = "First Java를 공부 중입니다.";
-        String var1, var2;
+        String var1, var2, var3;
 
         var1 = ss.toUpperCase();
         System.out.println(var1);
         var2 = ss.toLowerCase();
         System.out.println(var2);
+        var3 = ss.replaceAll(" ","");
+        System.out.println(var3);
     }
 }
