0df04a50be326509610a6c6f6a0d115b206c1e3f
syj6687 <syjsyj6687@naver.com>
2024-03-29 09:14:55 +0900

    2번째 파일

---
+++ src/main/java/Star03.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class star03 {
+
+    public static void main(String[] args) {
+        System.out.println("홀수를 입력해주세요");
+        Scanner sc = new Scanner(System.in);
+        int num = nextInt();
+
+        for (int i=0;i<num;i++){
+            for (int j=0; j<num;j++)
+            {
+                if (i<=num/2)
+                {
+                    if (i+j<=num/2-1)
+                        System.out.print(" ");
+                    else if (j-i>=num/2+1)
+                        System.out.print(" ");
+                    else
+                        System.out.print("*");
+                }
+                else if (i>num/2)
+                {
+                    if (i-j>=num/2+1)
+                        System.out.print(" ");
+                    else if (i+j>=num/2*3+1)
+                        System.out.print(" ");
+                    else
+                        System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
