856c7a2a44f65af560a7148cd691008fe5df1e4c
syj6687 <80371596+syj6687@users.noreply.github.com>
2024-04-19 10:40:36 +0900

    Create Array01.java

---
+++ src/main/java/Array01.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Array01 {
+    public static void main(String[] args) {
+        ArrayList pitches = new ArrayList();
+        pitches.add("10");
+        pitches.add("20");
+        pitches.add("30");
+    }
+}
